b050b48d4ca2873c01137088fb29f4162d9a8ea0
Adhyan Gupta <adhyangupta1999@gmail.com>
2022-03-09 14:02:02 +0530

    segragated username and user-email

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -7,27 +7,44 @@ module.exports = userModels = {
             await user.save()
             callback(undefined, user)
         } catch (error) {
+
             callback(error,undefined)
         }
 
     },
 
-    checkExistingUser: async (details, callback) => {
+    checkExistingUserName: async(details,callback)=>{
         try {
-            const uname = await User.find({ username: details.username })
-            const mail = await User.find({ email: details.email })
-            // console.log(uname)
-            if (uname.length>0) {
-                callback(undefined, 'username is already taken')
-            } else if (mail.length>0) {
-                callback(undefined,'email id alreaedy taken')
-            }
-            else callback(undefined, undefined)
-
+            let uname= await User.find({username: details.username} )
+            callback(undefined, uname)
         } catch (error) {
             callback(error,undefined)
-
         }
+    },
+    checkExistingUserEmail: async (details, callback)=>{
+        try {
+            let mail= await User.find({ email: details.email})
+            callback(undefined,mail)
+        } catch (error) {
+            callback(error, undefined)
+        }
+    },
 
-    }
+    // checkExistingUser: async (details, callback) => {
+    //     try {
+    //         let uname = await User.find({ username: details.username }).exec()
+    //         const mail = await User.find({ email: details.email }) 
+    //         if (uname.length>0) {
+    //             callback(undefined, 'username is already taken')
+    //         } else if (mail.length>0) {
+    //             callback(undefined,'email id alreaedy taken')
+    //         }
+    //         else callback(undefined, undefined)
+
+    //     } catch (error) {
+    //         callback(error,undefined)
+
+    //     }
+
+    // }
 }
