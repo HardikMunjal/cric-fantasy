78cba1ba0ed4aa2436b5e3eb95669a3553bb09cf
Adhyan Gupta <adhyangupta1999@gmail.com>
2022-03-14 17:22:32 +0530

    raised an event to log='home'

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,15 +1,19 @@
 // all the api(s) and the routes are to be written here
 const userController= require('../controllers/userController')
 const authController = require('../controllers/authController')
+const logController = require('../controllers/logController')
 
 
 
 module.exports= function(app){
-    app.get('',(req,res)=>{
+    app.get('/home',(req,res)=>{
+        logController.customEmitter.emit('addLog','home')
         res.send('Welcome to the registration page')
     })
     app.post('/authenticate',userController.validateCredential)
 
     app.get('/test',authController.validateToken) //just for token validation "testing" 
     app.post('/register',userController.checkExistingUserName,userController.checkExistingUserEmail,userController.createNewUser)
+
+    app.get('/viewlog',logController.Log.fetchLogEntries)
 }
